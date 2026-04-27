60fe3ccdb638ff19ffcff76f658524d68d0bfb48
Stephen Carder <stafngrimr@hotmail.co.uk>
2026-04-27 12:52:12 +0100

    added extended media buttons, broke the links though

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TOP Restaurant
 
 Project for the Odin Project building a restaurant website using webpack and javascript/css. No html.
+
+
--- src/index.js
+++ src/index.js
@@ -8,30 +8,43 @@ import homeActiveNotpressed from "./img/home_active_notpressed.png";
 import homeInactiveNotpressed from "./img/home_inactive_notpressed.png";
 import homeActivePressed from "./img/home_active_pressed.png";
 import homeInactivePressed from "./img/home_inactive_pressed.png";
+import menuActiveNotpressed from "./img/menu_active_notpressed.png";
+import menuInactiveNotpressed from "./img/menu_inactive_notpressed.png";
+import menuActivePressed from "./img/menu_active_pressed.png";
+import menuInactivePressed from "./img/menu_inactive_pressed.png";
+import contactActiveNotpressed from "./img/contact_active_notpressed.png";
+import contactInactiveNotpressed from "./img/contact_inactive_notpressed.png";
+import contactActivePressed from "./img/contact_active_pressed.png";
+import contactInactivePressed from "./img/contact_inactive_pressed.png";
 
 const homeButton = document.querySelector("#home");
-const menuButton = document.querySelector("button#menu");
-const contactButton = document.querySelector("button#contact");
+const menuButton = document.querySelector("#menu");
+const contactButton = document.querySelector("#contact");
 const content = document.querySelector("#content");
 
 const load = (function() {
     addHome();
+    homeButton.src = homeActiveNotpressed;
+    menuButton.src = menuInactiveNotpressed;
+    contactButton.src = contactInactiveNotpressed;
+
+    let page = "home";
 })();
 
 
 // Not the best way to implement the below. If statements aren't working? What?
 // the buttons are coming up, but need to work out BETTER logic for (in)active stuff
 //
+// Now the buttons arent' changing the links?
 
 
 homeButton.addEventListener("click", function() {
+    page = "home";
     homeButton.src = homeActiveNotpressed;
-    menuButton.style.color = "white";
-    menuButton.style.borderColor = "white";
-    contactButton.style.color = "white";
-    contactButton.style.borderColor = "white";
-    content.textContent = "";
+    menuButton.src = menuInactiveNotpressed;
+    contactButton.src = contactInactiveNotpressed;
 
+    content.textContent = "";
     addHome();
 });
 
@@ -44,21 +57,37 @@ homeButton.addEventListener("mouseleave", function() {
 });
 
 menuButton.addEventListener("click", function() {
+    page = "menu"
     homeButton.src = homeInactiveNotpressed;
-    menuButton.style.color = "red";
-    menuButton.style.borderColor = "red";
-    contactButton.style.color = "white";
-    contactButton.style.borderColor = "white";
+    menuButton.src = menuActiveNotpressed;
+    contactButton.src = contactInactiveNotpressed;
+
     content.textContent = "";
     addMenu();
 });
 
+menuButton.addEventListener("mouseenter", function() {
+    menuButton.src = menuActivePressed;
+});
+
+menuButton.addEventListener("mouseleave", function() {
+    menuButton.src = menuActiveNotpressed;
+});
+
 contactButton.addEventListener("click", function() {
+    page = "contact";
     homeButton.src = homeInactiveNotpressed;
-    menuButton.style.color = "white";
-    menuButton.style.borderColor = "white";
-    contactButton.style.color = "red";
-    contactButton.style.borderColor = "red";
+    menuButton.src = menuInactiveNotpressed;
+    contactButton.src = contactActiveNotpressed;
+
     content.textContent = "";
     addContact();
+});
+
+contactButton.addEventListener("mouseenter", function() {
+    contactButton.src = contactActivePressed;
+});
+
+contactButton.addEventListener("mouseleave", function() {
+    contactButton.src = contactActiveNotpressed;
 });

2587fa7e870a85405436f7be1d2a96d3c455aaf2
Stephen Carder <stafngrimr@hotmail.co.uk>
2026-04-29 13:36:18 +0100

    contact page updated

--- src/contact.js
+++ src/contact.js
@@ -5,11 +5,23 @@ export const addContact = function() {
     const container = document.querySelector("#content");
 
     const header = document.createElement("h1");
-    const desc = document.createElement("p");
+    const contentContainer = document.createElement("div");
+    const number = document.createElement("p");
+    const email = document.createElement("p");
+    const satellite = document.createElement("p");
+    const location = document.createElement("p");
 
-    header.textContent = "My Number";
-    desc.textContent = "You must be joking, right.";
+    header.textContent = "How to reach us";
+    contentContainer.setAttribute("id", "contact_container");
+    number.textContent = "📱 +e55 894f 48-e39 489fd a23";
+    email.textContent = "📨 admin@octodaddy.ven";
+    satellite.textContent = "🔲 145.825 - use call sign 'Daddy Menu'";
+    location.textContent = "🌌 52.4778195498919, -1.910826526728089, Venus";
 
     container.appendChild(header);
-    container.appendChild(desc);
+    container.appendChild(contentContainer);
+    contentContainer.appendChild(number);
+    contentContainer.appendChild(email);
+    contentContainer.appendChild(satellite);
+    contentContainer.appendChild(location);
 };

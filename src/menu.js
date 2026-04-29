f99ef3b76ec7faffa2200cb0d147abcc64882388
Stephen Carder <stafngrimr@hotmail.co.uk>
2026-04-29 17:59:35 +0100

    some work on menu, needs more

--- src/menu.js
+++ src/menu.js
@@ -1,27 +1,31 @@
 export const addMenu = function() {
+    
     const container = document.querySelector("#content");
     
+    // creating div for menu headers
     const tabDiv = document.createElement("div");
     tabDiv.setAttribute("id", "tabdiv");
     container.appendChild(tabDiv);
 
+    // creating div & elements for the menu items
     const menuDiv = document.createElement("div");
     menuDiv.setAttribute("id", "menudiv");
-    //menuDiv.textContent = "Please peruse the menu and give us a call to order via the contact page!";
+
     const image1 = document.createElement("div");
     image1.className = "menuItemImage";
-    //image1.setAttribute("id", "menuItemImage");
+    image1.setAttribute("id", "menuItem11");
+
     const image2 = document.createElement("div");
     image2.className = "menuItemImage";
-    //image1.setAttribute("id", "menuItemImage");
+    image2.setAttribute("id", "menuItem12");
+    
     const image3 = document.createElement("div");
     image3.className = "menuItemImage";
-    //image1.setAttribute("id", "menuItemImage");
+    image3.setAttribute("id", "menuItem13");
+
     const image4 = document.createElement("div");
     image4.className = "menuItemImage";
-    //image1.setAttribute("id", "menuItemImage");
-
-
+    image4.setAttribute("id", "menuItem14");
 
     menuDiv.appendChild(image1);
     menuDiv.appendChild(image2);
@@ -29,6 +33,7 @@ export const addMenu = function() {
     menuDiv.appendChild(image4);
     container.appendChild(menuDiv);
 
+    // creating content for tabs & setting up buttons
     let tabs = ["Seafood", "Chef", "Vegan", "Drinks"];
     let tabButtons = ["seaButton", "chefButton", "veganButton", "drinksButton"]
     for (let i=0; i<tabs.length; i++) {
@@ -41,34 +46,31 @@ export const addMenu = function() {
     let items = ["item1", "item2", "item3", "item4"];
     let descs = ["desc1", "desc2", "desc3", "desc4"]
 
+    // creating content for menu items
     const seaTab = function() {
         let itemContent = ["Power Noodles", "Shark Fin Soup", "Mussel Surprise", "Fish Heads on Toast"];
         let descContent = ["Crab based noodle dish, with Spring Onions & Chilli Peppers", "A flavorful soup made from Shark's Fins, complimented by aromatic garnishing", "A bucket of Mussels, what could be better!", "The best part of the Fish on some Toast. To recall simpler times in life"];
 
-        for(let i=0; i<items.length; i++) {
-            items[i] = document.createElement("h2");
-            items[i].textContent = itemContent[i];
-            menuDiv.appendChild(items[i]);
+        image1.setAttribute("id", "menuItem11");
+        image2.setAttribute("id", "menuItem12");
+        image3.setAttribute("id", "menuItem13");
+        image4.setAttribute("id", "menuItem14");
 
-            descs[i] = document.createElement("p");
-            descs[i].textContent = descContent[i];
-            menuDiv.appendChild(descs[i]);
-        }
+        image1.addEventListener("mouseenter", function() {
+            image1.textContent = itemContent[0];
+            console.log("enter");
+        });
+
+        image1.addEventListener("mouseleave", function() {
+            image1.textContent = "";
+            console.log("leave");
+        });
     };
 
     const chefTab = function() {
         let itemContent = ["Legs & Mash", "Eyes Surprise", "Human Bacon Sandwich", "Sunday Roast"];
         let descContent = ["Delicious and sumtuous human leg, served on a bed of mashed potatoes, covered in gravy", "A bucket of Human eyes! Juicier even than the mussels!", "Nobody mourns the extinction of the pig since this creation", "Old Human tradition from what was north 'Europe' of roasting meat on Sunday."];
-
-        for(let i=0; i<items.length; i++) {
-            items[i] = document.createElement("h2");
-            items[i].textContent = itemContent[i];
-            menuDiv.appendChild(items[i]);
-
-            descs[i] = document.createElement("p");
-            descs[i].textContent = descContent[i];
-            menuDiv.appendChild(descs[i]);
-        };
+s
     };
 
     const veganTab = function() {
@@ -90,6 +92,8 @@ export const addMenu = function() {
         const itemContent = ["drinks 1", "2", "3", "4"];
         const descContent = ["First one", "Second one", "Third one", "Fourth one"];
 
+
+    // 
         for(let i=0; i<items.length; i++) {
             items[i] = document.createElement("h2");
             items[i].textContent = itemContent[i];
@@ -101,22 +105,57 @@ export const addMenu = function() {
         };
     };
 
+    // SEAFOOD onclick
     tabButtons[0].addEventListener("click", function() {
+        tabButtons.forEach(function(button) {
+            if (button === tabButtons[0]) {
+                button.classList.add("active");
+            } else {
+                button.classList.remove("active");
+            }
+        });
+
         menuDiv.textContent = "";
         seaTab();
     });
 
+    // CHEF onclick
     tabButtons[1].addEventListener("click", function() {
+        tabButtons.forEach(function(button) {
+            if (button === tabButtons[1]) {
+                button.classList.add("active");
+            } else {
+                button.classList.remove("active");
+            }
+        });
         menuDiv.textContent = "";
         chefTab();
     });
 
+    // VEGAN onclick
     tabButtons[2].addEventListener("click", function() {
+        tabButtons.forEach(function(button) {
+            if (button === tabButtons[2]) {
+                button.classList.add("active");
+            } else {
+                button.classList.remove("active");
+            }
+        });
+
         menuDiv.textContent = "";
         veganTab();
     });
 
+    // DRINKS onclick
     tabButtons[3].addEventListener("click", function() {
+        tabButtons.forEach(function(button) {
+            if (button === tabButtons[3]) {
+                button.classList.add("active");
+            } else {
+                button.classList.remove("active");
+            }
+        });
+
         menuDiv.textContent = "";
         drinksTab();
     });

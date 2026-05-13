426d1dd8b47a23e6e6be4f9e98818a2b79150d4b
Stephen Carder <stafngrimr@hotmail.co.uk>
2026-05-13 22:14:49 +0100

    finish menu items

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,18 @@ import item11 from "./img/1-1.png";
 import item12 from "./img/1-2.png";
 import item13 from "./img/1-3.png";
 import item14 from "./img/1-4.png";
+import item21 from "./img/2-1.png";
+import item22 from "./img/2-2.png";
+import item23 from "./img/2-3.png";
+import item24 from "./img/2-4.png";
+import item31 from "./img/3-1.png";
+import item32 from "./img/3-2.png";
+import item33 from "./img/3-3.png";
+import item34 from "./img/3-4.png";
+import item41 from "./img/4-1.png";
+import item42 from "./img/4-2.png";
+import item43 from "./img/4-3.png";
+import item44 from "./img/4-4.png";
 
 export const addMenu = function() {
     const container = document.querySelector("#content");
@@ -19,7 +31,6 @@ export const addMenu = function() {
     cont1.className = "menuContainers";
     const image1 = document.createElement("img");
     image1.className = "menuItemImage";
-    image1.src = item11;
     const under1 = document.createElement("div");
     under1.className = "underlay";
 
@@ -27,7 +38,6 @@ export const addMenu = function() {
     cont2.className = "menuContainers";
     const image2 = document.createElement("img");
     image2.className = "menuItemImage";
-    image2.src = item12;
     const under2 = document.createElement("div");
     under2.className = "underlay";
     
@@ -35,7 +45,6 @@ export const addMenu = function() {
     cont3.className = "menuContainers";
     const image3 = document.createElement("img");
     image3.className = "menuItemImage";
-    image3.src = item13;
     const under3 = document.createElement("div");
     under3.className = "underlay";
 
@@ -43,7 +52,6 @@ export const addMenu = function() {
     cont4.className = "menuContainers";
     const image4 = document.createElement("img");
     image4.className = "menuItemImage";
-    image4.src = item14;
     const under4 = document.createElement("div");
     under4.className = "underlay";
 
@@ -80,14 +88,31 @@ export const addMenu = function() {
     // array for iterating over underlays
     const unders = [under1, under2, under3, under4]
 
+    // initial text & imagery
+    let itemContent = ["Power Noodles", "Shark Fin Soup", "Mussel Surprise", "Fish Heads on Toast"];
+    image1.src = item11;
+    image2.src = item12;
+    image3.src = item13;
+    image4.src = item14;
+
+    for (let i = 0; i < unders.length; i++) {
+        unders[i].addEventListener("mouseenter", function() {
+            unders[i].textContent = itemContent[i];
+        });
+
+        unders[i].addEventListener("mouseleave", function() {
+            unders[i].textContent = "";
+        });
+    }
+
     // content for menu items
     const seaTab = function() {
-        let itemContent = ["Power Noodles", "Shark Fin Soup", "Mussel Surprise", "Fish Heads on Toast"];
+        itemContent = ["Power Noodles", "Shark Fin Soup", "Mussel Surprise", "Fish Heads on Toast"];
 
-        image1.setAttribute("id", "menuItem11");
-        image2.setAttribute("id", "menuItem12");
-        image3.setAttribute("id", "menuItem13");
-        image4.setAttribute("id", "menuItem14");
+        image1.src = item11;
+        image2.src = item12;
+        image3.src = item13;
+        image4.src = item14;
 
         for (let i = 0; i < unders.length; i++) {
             unders[i].addEventListener("mouseenter", function() {
@@ -95,18 +120,18 @@ export const addMenu = function() {
             });
 
             unders[i].addEventListener("mouseleave", function() {
-                unders[i].textContent = itemContent[i];
+                unders[i].textContent = "";
             });
         }
     };
 
     const chefTab = function() {
-        let itemContent = ["Legs & Mash", "Eyes Surprise", "Human Bacon Sandwich", "Sunday Roast"];
+        itemContent = ["Legs & Mash", "Eyes Surprise", "Human Bacon Sandwich", "Sunday Roast"];
 
-        image1.setAttribute("id", "menuItem21");
-        image2.setAttribute("id", "menuItem22");
-        image3.setAttribute("id", "menuItem23");
-        image4.setAttribute("id", "menuItem24");
+        image1.src = item21;
+        image2.src = item22;
+        image3.src = item23;
+        image4.src = item24;
 
         for (let i = 0; i < unders.length; i++) {
             unders[i].addEventListener("mouseenter", function() {
@@ -114,18 +139,18 @@ export const addMenu = function() {
             });
 
             unders[i].addEventListener("mouseleave", function() {
-                unders[i].textContent = itemContent[i];
+                unders[i].textContent = "";
             });
         }
     };
 
     const veganTab = function() {
-        const itemContent = ["Seaweed Salad", "Mushroom Risotto", "Grass Tower", "Algae Soup"];
+        itemContent = ["Seaweed Salad", "Mushroom Risotto", "Grass Tower", "Algae Soup"];
 
-        image1.setAttribute("id", "menuItem31");
-        image2.setAttribute("id", "menuItem32");
-        image3.setAttribute("id", "menuItem33");
-        image4.setAttribute("id", "menuItem34");
+        image1.src = item31;
+        image2.src = item32;
+        image3.src = item33;
+        image4.src = item34;
 
         for (let i = 0; i < unders.length; i++) {
             unders[i].addEventListener("mouseenter", function() {
@@ -133,18 +158,18 @@ export const addMenu = function() {
             });
 
             unders[i].addEventListener("mouseleave", function() {
-                unders[i].textContent = itemContent[i];
+                unders[i].textContent = "";
             });
         }
     };
 
     const drinksTab = function() {
-        const itemContent = ["Hot Drinks", "Salt-water", "18 Year Scotch", "Ink Soda"];
+        itemContent = ["Hot Drinks", "Salt-water", "18 Year Scotch", "Ink Soda"];
 
-        image1.setAttribute("id", "menuItem41");
-        image2.setAttribute("id", "menuItem42");
-        image3.setAttribute("id", "menuItem43");
-        image4.setAttribute("id", "menuItem44");
+        image1.src = item41;
+        image2.src = item42;
+        image3.src = item43;
+        image4.src = item44;
 
         for (let i = 0; i < unders.length; i++) {
             unders[i].addEventListener("mouseenter", function() {
@@ -152,7 +177,7 @@ export const addMenu = function() {
             });
 
             unders[i].addEventListener("mouseleave", function() {
-                unders[i].textContent = itemContent[i];
+                unders[i].textContent = "";
             });
         }
     };
